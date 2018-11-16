3de20603c9c241e5b771b0b0458629c9137be16f
Quang Nguyen <nguyenq@sedsystems.ca>
2018-11-16 14:41:15 -0600

    Item 01

---
+++ Item01/Item01_FederalCppHello/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Welcome to Cpp - the Federal languanges: " << endl;
+    cout << "\t 1. C \n"
+         << "\t 2. Object Oriengted C: \n"
+         << "\t\t 2.1 Classes (constructors and destructors)" << endl
+         << "\t\t 2.2 Encapsulation" << endl
+         << "\t\t 2.3 Inheritance"<< endl
+         << "\t\t 2.4 Polymorphism"<< endl
+         << "\t\t 2.5 Virtual functions (dynamic binding)" << endl
+         << "\t 3. Template C : TMP Template Metaprogramming" << endl
+         << "\t 4. STL" << endl;
+
+    cout << endl << "------------------------------------------" << endl;
+
+    return 0;
+}
+
